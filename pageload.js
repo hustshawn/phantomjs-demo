cda71da7f2059936c146a9b786f71cce3f6388e2
hustshawn <hustshawn@gmail.com>
2016-12-04 17:23:57 +0800

    Added the snapshot configuration.

--- pageload.js
+++ pageload.js
@@ -1,5 +1,12 @@
 var page = require('webpage').create();
-page.open('http://www.douban.com', function(status) {
+var system = require('system');
+// the actual ize of the headless browser
+page.viewportSize = {width: 1024, height: 768};
+// the cliprect is the portion of the page your are takeing snapshot of
+page.clipRect = { top: 0, left: 0, width: 1024, height:768 };
+
+var url = system.args[1] ? system.args[1] : 'http://www.douban.com';
+page.open(url, function(status) {
   console.log('Status:', status);
   if (status === 'success') {
     page.render('page_snapshot.png');
