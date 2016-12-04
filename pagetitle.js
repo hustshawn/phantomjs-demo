84d42b10612cd0bd2240f1a5b6e3f33c6bb5ff25
hustshawn <hustshawn@gmail.com>
2016-12-04 17:05:22 +0800

    Page title

---
+++ pagetitle.js
@@ -0,0 +1,22 @@
+var page = require('webpage').create(),
+  system = require('system');
+
+if (system.args.length === 1) {
+  console.log("Usage: loadspeed.js <URL>");
+  phantom.exit();
+}
+
+var url = system.args[1];
+
+page.open(url, function(status) {
+  if (status !== 'success') {
+    console.log("Failed to load the resource, exit somehow");
+    phantom.exit();
+  }
+  var title = page.evaluate(function() {
+    return document.title;
+  });
+  console.log("Requested page: ", url);
+  console.log("Page titile: ", title);
+  phantom.exit();
+});
